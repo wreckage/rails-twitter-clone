09a1486482f5b8eb0af24c8bb7bc4afd48008600
Reuben Urbina <reubenurbina@gmail.com>
2017-01-02 17:47:25 -0800

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # virtual attribute w/ both getter & setter
-    attr_accessor :remember_token, :activation_token 
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
@@ -51,6 +51,26 @@ class User < ApplicationRecord
     # Sends activation email.
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns(reset_digest: User.digest(reset_token), 
+                       reset_sent_at: Time.zone.now)
+
+    end
+
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    # Returns true if a password reset has expired
+    # Note it's not 'self.reset_sent_at', because for reads Ruby will check
+    # local variables, and if finds none matching will go up a level and
+    # check there (in this case, goes up to the user instance). See:
+    # http://stackoverflow.com/a/20738162
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
